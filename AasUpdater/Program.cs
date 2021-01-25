6511384621e08a6269284d4d9c0bedbb5f725784
Jerry Hang <superjerry88@hotmail.com>
2021-01-25 16:00:51 +0800

    Add project files.

---
+++ AasUpdater/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AasUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Logic.CheckPathExist());
+            Console.Read();
+        }
+    }
+
+    public class Logic
+    {
+        public static bool CheckPathExist()
+        {
+           return Directory.Exists(Config.GatewayPath);
+        }
+    }
+
+    public class Config
+    {
+        public const string GatewayPath = @"C:\AAS";
+        public const string ControlPanelPath = @"C:\ControlPanel";
+    }
+}
